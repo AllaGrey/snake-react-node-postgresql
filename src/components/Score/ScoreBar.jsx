578da750a6c1d751b21ff9734c410cbfae9b6ef8
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2023-11-14 10:05:46 +0200

    Update ScoreBar.jsx

--- src/components/Score/ScoreBar.jsx
+++ src/components/Score/ScoreBar.jsx
@@ -13,7 +13,7 @@ export const ScoreBar = ({ scoresData }) => {
                         <TopScores key={index}>
                             <ScoreType>{index + 1}. {score.user}: <Score>{score.score}</Score></ScoreType>
                         </TopScores>)}
-                    <ScoreType>Personal best score: <Score>{userMaxScore.score}</Score></ScoreType>
+                    <ScoreType>Personal best score: <Score>{userMaxScore ? userMaxScore : '0'}</Score></ScoreType>
                 </ScoreBarContainer>
             }
         </>
@@ -23,10 +23,11 @@ export const ScoreBar = ({ scoresData }) => {
 ScoreBar.propTypes = {
     scoresData: PropTypes.shape({
         data: PropTypes.shape({
-            topScores: PropTypes.array.isRequired,
-            userMaxScore: PropTypes.shape({
+            topScores: PropTypes.arrayOf(PropTypes.shape({
+                user: PropTypes.string.isRequired,
                 score: PropTypes.number.isRequired,
-            }).isRequired,
+            })).isRequired,
+            userMaxScore: PropTypes.number.isRequired,
         }).isRequired,
-    })
-}
+    }).isRequired,
+};
